0487209d835c795d45a9565c7f45cc977008f1b4
lkralong96-a11y <lkralong96@gmail.com>
2026-03-18 14:47:38 +0530

    Delete HostelManagement-main directory

--- HostelManagement-main/routes/publicRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const publicController = require('../controllers/publicController');
-
-// No auth middleware — these routes are public
-router.get('/hostels', publicController.getHostelAvailability);
-router.get('/hostels/available', publicController.getAvailableHostels);
-
-module.exports = router;
